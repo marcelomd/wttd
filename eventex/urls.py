22012eb1cd20408581daf2d9e9e249ff6039da0d
Marcelo MD <mallmanndias@gmail.com>
2013-12-20 18:56:32 -0200

    URL refactor 1

--- eventex/urls.py
+++ eventex/urls.py
@@ -4,9 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', 'eventex.core.views.home', name='name'),
-    url(r'^inscricao/$', 'eventex.subscriptions.views.subscribe', name='subscribe'),
-    url(r'^inscricao/(\d+)/$', 'eventex.subscriptions.views.detail', name='detail'),
-
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^inscricao/', include('eventex.subscriptions.urls', namespace='subscriptions')),
+    url(r'', include('eventex.core.urls', namespace='core')),
 )
