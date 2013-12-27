af052c0b5909b6f670e59865aaae123ab3b84012
Marcelo MD <mallmanndias@gmail.com>
2013-12-27 12:59:49 -0200

    Media

--- eventex/core/urls.py
+++ eventex/core/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('eventex.core.views',
         url(r'^palestrantes/(?P<slug>[\w-]+)/$', 'speaker_detail', name='speaker_detail'),
+        url(r'^palestras/(?P<pk>\d+)/$', 'talk_detail', name='talk_detail'),
         url(r'^palestras/$', 'talk_list', name='talk_list'),
         url(r'^$', 'home', name='home'),
 )
